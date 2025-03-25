9fdb8115c134c941ce73b50cde5b8cd2096e50ce
RunTuTuTu <1205657408@qq.com>
2025-03-25 19:10:47 +0800

    3

--- 1_100/3.py
+++ 1_100/3.py
@@ -14,6 +14,7 @@ class Solution(object):
                 # 能进到这里面，要么就是右指针到最右边，要么就是右指针遇到重复的元素
                 res_set.remove(s[i-1])
             while right < len(s) and s[right] not in res_set:
+                # 循环，如果右指针没有遇到重复的元素，就一直循环，并且把右指针的值加到集合中
                 res_set.add(s[right])
                 right += 1
                 res_len = max(res_len, right - i)
